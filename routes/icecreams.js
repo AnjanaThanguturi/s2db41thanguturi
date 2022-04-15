d96c62e82a2c1146fd2773bbfa3f17901bd044ad
AnjanaThanguturi <anjanathanguturi@gmail.com>
2022-04-15 15:50:03 -0500

    part 4 updated

--- routes/icecreams.js
+++ routes/icecreams.js
@@ -4,4 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', icecream_controllers.icecream_view_all_Page);
+/* GET detail icecream page */
+router.get('/detail', icecream_controllers.icecream_view_one_Page);
 module.exports = router;
