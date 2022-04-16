55279be1712ab12a7d4e38962f1baa6e72db9c32
AnjanaThanguturi <anjanathanguturi@gmail.com>
2022-04-16 15:44:58 -0500

    part 6 updated

--- routes/icecreams.js
+++ routes/icecreams.js
@@ -11,6 +11,9 @@ router.get('/detail', icecream_controllers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controllers.icecream_create_Page);
 
+/* GET create update page */
+router.get('/update', icecream_controllers.icecream_update_Page);
+
 
 
 module.exports = router;
