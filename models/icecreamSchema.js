29850ec29cb438b2160473563f3e529e90a23048
AnjanaThanguturi <anjanathanguturi@gmail.com>
2022-04-24 17:04:35 -0500

    bonus updated

--- models/icecreamSchema.js
+++ models/icecreamSchema.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
-icecream_flavour: String,
-icecream_quantity: String,
-icecream_cost: {
-    type: Number,
-    min: 2,
-    max: 30
-  } 
+    icecream_flavour: {
+        type: String,
+        required: true
+    },
+    icecream_quantity: {
+        type: String,
+        required: true
+    },
+    icecream_cost: {
+        type: Number,
+        min: 2,
+        max: 30
+    }
 })
-module.exports = mongoose.model("Icecream",icecreamSchema)
+module.exports = mongoose.model("Icecream", icecreamSchema)
