e517ff7b2eaf7e29022122703362e6c9ade6e8e9
AnjanaThanguturi <anjanathanguturi@gmail.com>
2022-04-24 13:52:16 -0500

    part 5 updated

--- routes/icecreams.js
+++ routes/icecreams.js
@@ -1,6 +1,13 @@
 var express = require('express');
-const icecream_controllers= require('../controllers/icecream');
+const icecream_controllers = require('../controllers/icecream');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', icecream_controllers.icecream_view_all_Page);
@@ -12,7 +19,7 @@ router.get('/detail', icecream_controllers.icecream_view_one_Page);
 router.get('/create', icecream_controllers.icecream_create_Page);
 
 /* GET create update page */
-router.get('/update', icecream_controllers.icecream_update_Page);
+router.get('/update', secured, icecream_controllers.icecream_update_Page);
 
 /* GET delete delete page */
 router.get('/delete', icecream_controllers.icecream_delete_Page);
